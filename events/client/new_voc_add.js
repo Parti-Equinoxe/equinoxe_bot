0a1808efb5781b66811c2b87c4612bb76dfc718d
Youri Duret <youri.duret@gmail.com>
2024-08-30 15:56:17 +0200

    ajout channel link

--- events/client/new_voc_add.js
+++ events/client/new_voc_add.js
@@ -9,13 +9,16 @@ client.on("voiceStateUpdate", async (oldState, newState) => {
         reason:`Nouveau salon vocal depuis <#${newState.channelId}>`,
     });
     await newState.member.voice.setChannel(voc.id, `Nouveau salon vocal depuis <#${newState.channelId}>`);
+    await voc.send({
+        content: `:speaker:Vous pouvez inviter des gens dans le vocal avec : \`\`\`\n<#${voc.id}>\`\`\``
+    })
     const button = new ButtonBuilder()
         .setCustomId("accueil:renomervoc")
         .setLabel("Renommer")
         .setStyle(2)
         .setEmoji("📝");
     await voc.send({
-        content: `<@${newState.member.user.id}>, vous pouvez renomer le salon en utilisant le bouton.`,
+        content: `<@${newState.member.user.id}>, vous pouvez renomer le salon en utilisant le bouton :`,
         components: [new ActionRowBuilder().addComponents(button)]
     });
 })
