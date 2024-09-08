9525bf16e524600a29b349c7a5c75983e1dd9d08
Youri Duret <youri.duret@gmail.com>
2024-09-08 19:38:10 +0200

    ajout cmd archiver (crash)

---
+++ interactions/commands/gestion/archive_channel.js
@@ -0,0 +1,43 @@
+const categories = require("../../../data/utils/categories.json");
+const {ChannelType} = require("discord.js");
+
+module.exports = {
+    name: "archive",
+    description: "Permet d'archiver un salons.",
+    options: [{
+        name: "salon",
+        description: "Salon à archiver.",
+        type: 7,
+        required: true,
+        channelTypes: [0, 5]
+    }],
+    devOnly: true,
+    /**
+     * @param {ChatInputCommandInteraction} interaction
+     * @param {Client} client
+     */
+    runInteraction: async (client, interaction) => {
+        const channel = interaction.options.getChannel("salon");
+        /*if (channel.) return interaction.reply({
+            content: ":page_facing_up: Le salon doit être un salon texte !",
+            ephemeral: true
+        });*/
+        if (channel.parentId === categories.archive) return interaction.reply({
+            content: ":page_facing_up: Ce salon est déjà archivé.",
+            ephemeral: true
+        });
+        await channel.setParent(categories.archive, {lockPermissions: true});
+        //await channel.lockPermissions();
+        await channel.send({
+            content: `:lock: Le salon a été archivé par **${interaction.user.username}** le <t:${Math.floor(Date.now() / 1000)}:f>.`
+        });
+        /*await api.utils.log({
+            content: `:lock: Le salon <#${channel.id}> a été archiver par <@${interaction.user.id}>`,
+            allowedMentions: {repliedUser: false}
+        });*/
+        return interaction.reply({
+            content: `:lock: Le salon <#${channel.id}> a bien été archivé.`,
+            ephemeral: true
+        });
+    }
+};
