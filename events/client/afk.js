b5fcb36ffb54833f0735a108b69eafc2d54c4df0
Youri Duret <youri.duret@gmail.com>
2024-09-10 20:00:17 +0200

    Events

--- events/client/afk.js
+++ events/client/afk.js
@@ -1,6 +1,7 @@
 const {salons} = require("../../api/permanent.js");
 const client = require("../../index").client;
-client.on("voiceStateUpdate", async (oldState, newState) => {
+const { Events } = require('discord.js');
+client.on(Events.VoiceStateUpdate, async (oldState, newState) => {
     if (newState.channelId !== salons.afk) return;
     await newState.disconnect("Le membre est en AFK !");
 })
