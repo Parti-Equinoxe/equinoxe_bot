ff45565bdb5e4721673fd03cbc2f9416eb164667
Youri Duret <youri.duret@gmail.com>
2024-09-15 19:08:01 +0200

    taff com

--- data/pre_load/infoCom.js
+++ data/pre_load/infoCom.js
@@ -11,15 +11,15 @@ const {banniere, couleurs, roles} = require("../../api/permanent.js");
  * @param {ChatInputCommandInteraction} interaction
  */
 module.exports.send = async (interaction) => {
-    const contri = new AttachmentBuilder('./data/images/contribuer.png', {name: 'contribuer.png'});
+    const info = new AttachmentBuilder('./data/images/information.png', {name: 'information.png'});
     const texte = readFileSync("./data/textes/infoCom.txt", "utf-8");
-    console.log(texte);
+    //console.log(texte);
     return {
         embeds: [new EmbedBuilder()
             .setDescription(texte)
             .setColor(couleurs.jaune)
             .setThumbnail(banniere.link)
-            .setImage(`attachment://contribuer.png`)
+            .setImage(`attachment://information.png`)
             /*.addFields([{
                 name: ":mobile_phone: Réseaux sociaux :",
                 value: "- Créer des posts pour faire connaitre Équinoxe, informer et partager.\n- Animer en répondant au commentaires."
@@ -31,6 +31,6 @@ module.exports.send = async (interaction) => {
                 value: "- Créer le contenu partager sur les platformes.\n- Créer les affiches et les tracts pour les campagnes.\n- Monter les vidéos et les podcasts."
             }])*/
             /*.setFooter({text: "Cliquez sur les boutons ci-dessous pour vous ajouter/retirer des roles."})*/],
-        files: [banniere.file(), contri]
+        files: [banniere.file(), info]
     };
 }
