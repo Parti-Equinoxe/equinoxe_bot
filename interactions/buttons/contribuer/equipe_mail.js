b9e38eccf473e1129a9dca0e7e973cf17689d1b5
Youri Duret <youri.duret@gmail.com>
2024-09-15 18:05:57 +0200

    rôle sup

--- interactions/buttons/contribuer/equipe_mail.js
+++
@@ -1,8 +0,0 @@
-const {rolereact} = require("../../../api/role.js");
-const {roles} = require("../../../api/permanent.js");
-module.exports = {
-    customID: "equipe_mail",
-    runInteraction: async (client, interaction) => {
-        return rolereact(interaction, roles.equipe_mail);
-    }
-}
