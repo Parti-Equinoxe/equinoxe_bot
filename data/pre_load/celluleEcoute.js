3db282497df8fdc88ea7f6807b8d6f4ea4b8fc35
Youri Duret <youri.duret@gmail.com>
2024-09-20 14:08:10 +0200

    ajout msg cellule d'ecoute

---
+++ data/pre_load/celluleEcoute.js
@@ -0,0 +1,26 @@
+const {
+    EmbedBuilder,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ChatInputCommandInteraction, AttachmentBuilder
+} = require("discord.js");
+const {banniere, couleurs} = require("../../api/permanent.js");
+/**
+ * @param {ChatInputCommandInteraction} interaction
+ */
+module.exports.send = async (interaction) => {
+    const info = new AttachmentBuilder('./data/images/information.png', {name: 'info.png'});
+
+    return {
+        embeds: [
+            //.setFooter({text: ""}),
+            new EmbedBuilder()
+                .setDescription("# __Cellule d'écoute :__\n- Si vous êtes victime de comportements inappropriés.\n- Si vous êtes témoin de comportements inappropriés.\n### Vous pouvez contacter <@1255487701109706816> ou le \`06 71 42 93 63\`, l'anonymat sera respecté.")
+                .setColor(couleurs.jaune)
+                .setThumbnail(banniere.link)
+                .setImage(`attachment://info.png`)
+            //.setFooter({text: ""})
+        ],
+        files: [banniere.file(), info]
+    };
+}
