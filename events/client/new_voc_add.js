87f449b9d999e58b8e00aca07b010dafb1ea281d
Youri Duret <youri.duret@gmail.com>
2024-09-20 15:53:32 +0200

    add save msg

--- events/client/new_voc_add.js
+++ events/client/new_voc_add.js
@@ -11,7 +11,7 @@ client.on(Events.VoiceStateUpdate, async (oldState, newState) => {
     });
     await newState.member.voice.setChannel(voc.id, `Nouveau salon vocal depuis <#${newState.channelId}>`);
     await voc.send({
-        content: `:speaker: Vous pouvez inviter des gens dans le vocal avec : \`\`\`\n<#${voc.id}>\`\`\`\nCe vocal se supprime automatiquement lorsque tout le monde l'aura quitter.\nAttention ce salon sera supprimer (*lorsque tout le monde aura quitter*) alors n'oubliez rien dans la discussion !`,
+        content: `:speaker: Vous pouvez inviter des gens dans le vocal avec : \`\`\`\n<#${voc.id}>\`\`\`\nCe vocal se supprime automatiquement lorsque tout le monde l'aura quitter.\nAttention ce salon sera supprimer (*lorsque tout le monde aura quitter*) alors n'oubliez rien dans la discussion (Vous pouvez demandez la sauvegarde des 100 derniers message au modérateurs) !`,
     })
     const button = new ButtonBuilder()
         .setCustomId("accueil:renomervoc")
--- events/client/new_voc_delete.js
+++ events/client/new_voc_delete.js
@@ -1,8 +1,20 @@
 const {salons} = require("../../api/permanent.js");
 const {Events} = require("discord.js");
+const discordTranscripts = require("discord-html-transcripts");
+const {logWithImage, durationFormatter} = require("../../api/utils");
 const client = require("../../index").client;
 client.on(Events.VoiceStateUpdate, async (oldState, newState) => {
     if (oldState.channel == null || !oldState.channel.name.includes("🔈")) return;
     if (oldState.channel.members.size !== 0) return;
+    const channel = oldState.channel;
+    await channel.messages.fetch({force: true, limit: 100});
+    console.log(channel.messages.cache.map((m)=>m).length);
+    const nb= Math.min(channel.messages.cache.map((m)=>m).length, 100);
+    const msgs = channel.messages.cache.sort((a, b) => b.createdTimestamp - a.createdTimestamp)
+        .filter((msg) => !(msg.content === "" && msg.embeds.length === 0 && msg.attachments.size === 0))
+        .map((msg) => msg).slice(0, nb)
+        .sort((a, b) => a.createdTimestamp - b.createdTimestamp);
+    const attachment = await discordTranscripts.generateFromMessages(msgs, channel);
+    await logWithImage(`Sauvegarde des dernier **${msgs.length}** messages du vocal **${channel.name}**.`, "Sauvegarde Vocal Automatique", oldState.member, "success", attachment);
     await oldState.channel.delete();
 });
