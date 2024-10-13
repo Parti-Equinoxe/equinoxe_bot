e5ce67e482f51aa1e6227149e3789adc36abfa5a
Youri Duret <youri.duret@gmail.com>
2024-10-13 22:36:04 +0200

    roles programme

--- interactions/buttons/contribuer/prog_formation.js
+++ interactions/buttons/contribuer/prog_formation.js
@@ -1,8 +1,8 @@
 const {roles} = require("../../../api/permanent.js");
 const {rolereact} = require("../../../api/role.js");
 module.exports = {
-    customID: "prof_formation",
+    customID: "prog_formation",
     runInteraction: async (client, interaction) => {
-        return rolereact(interaction, roles.prof_formation);
+        return rolereact(interaction, roles.prog_formation);
     }
 }
