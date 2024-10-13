f2fd00182cdcc349108dc369b10c2da7025ef458
Youri Duret <youri.duret@gmail.com>
2024-10-13 22:23:23 +0200

    fix ? crash

--- interactions/buttons/autre/sup_msg_ref.js
+++ interactions/buttons/autre/sup_msg_ref.js
@@ -7,6 +7,7 @@ module.exports = {
     runInteraction: async (client, interaction) => {
         if (!interaction.message.reference) return;
         const msg = await interaction.channel.messages.fetch(interaction.message.reference.messageId);
+        if (!msg) return;
         if (interaction.user.id !== msg.author.id) return interaction.reply({
             content: `:no_entry_sign: Vous n'avez pas la permission de supprimer ce message.`,
             ephemeral: true
