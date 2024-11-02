c2c511f899e19371fdf7c0cab6b5f2d32e92adb5
Youri Duret <youri.duret@gmail.com>
2024-11-02 19:27:53 +0100

    ajout /deputé

---
+++ interactions/commands/parlement/parlement.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: "parlement",
+    description: "Permet d'obtenir des informations sur le parlement et les députés.",
+    commandeGroupe: true,
+    category: "information",
+}
