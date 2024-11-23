e3c00a431519bf0b1b78d52491628609477ed71b
Youri Duret <youri.duret@gmail.com>
2024-11-23 19:33:00 +0100

    clean up

--- src/interactions/buttons/contribuer/equipe_linkedin.js
+++
@@ -1,8 +0,0 @@
-const {roles} = require("../../../api/permanent.js");
-const {rolereact} = require("../../../api/role.js");
-module.exports = {
-    customID: "equipe_linkedin",
-    runInteraction: async (client, interaction) => {
-        return rolereact(interaction, roles.equipe_linkedin);
-    }
-}
