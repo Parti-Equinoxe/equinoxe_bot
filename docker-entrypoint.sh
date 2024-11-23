8ebff9dab78fddc887208cf8024d1664db025d57
Baptiste Rajaut <rajaut@bayesia.com>
2024-11-23 18:12:36 +0100

    dockerfile : use loop to always update bot on restart

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -ue pipefail
 git clone https://github.com/Parti-Equinoxe/equinoxe_bot /equinoxe/bot
-cd /equinoxe/bot/src
+
 
 while true; do
+  cd /equinoxe/bot
+  git pull
+  cd src
   npm install
-  npm ci
   node index.js
   sleep 5
 done
