3991a6cf2137addffd488ff34297c9ce92bd4621
Youri Duret <youri.duret@gmail.com>
2024-12-02 22:58:38 +0100

    test calendar

--- src/interactions/commands/utils/test.js
+++ src/interactions/commands/utils/test.js
@@ -1,21 +1,46 @@
-const {EmbedBuilder} = require("discord.js");
+const calendarConfig = require("../../../data/utils/calendar.json");
+const {getEvents, embedEvent, rappel} = require("../../../api/google");
+const {salons} = require("../../../api/permanent");
+const choices = calendarConfig.list.map((c) => {
+    return {
+        name: c.name,
+        value: c.id
+    }
+});
+
 module.exports = {
     name: "test",
     description: "pour faire des test",
     devOnly: true,
+    options: [
+        {
+            name: "nom",
+            description: "Le nom de l'équipe ou du calendrier à afficher.",
+            type: 3,
+            choices: choices,
+            required: true
+        }
+    ],
     runInteraction: async (client, interaction) => {
-        const embed = new EmbedBuilder()
-            .setTitle("test")
-            .setColor("#ffd412")
-            .setDescription("test")
-            .setFields([
-                {
-                    name: "test",
-                    value: "test"
-                }
-            ])
-            .setTimestamp();
-        console.log(embed.toJSON());
-        return interaction.reply("test!");
+        await interaction.deferReply({ephemeral: true});
+        /*const timeMin = new Date();
+        const decalage = 8 - timeMin.getDay();
+        timeMin.setDate(timeMin.getDate());
+        const timeMax = new Date();
+        timeMax.setDate(timeMax.getDate() + decalage === 8 ? 1 : decalage);
+        const events = await getEvents(interaction.options.getString("nom"), timeMin, timeMax);
+        if (events.length === 0) return interaction.editReply({
+            content: ":x: Aucune reunion trouvée !",
+            ephemeral: true
+        });
+        console.log(events);
+        const channel = await interaction.guild.channels.fetch(salons[events[0].calendar.channel]);
+        const embeds = events.slice(0, 10).map(cal => embedEvent(cal))
+        channel.send({embeds: embeds});*/
+        await rappel()
+        return interaction.editReply({
+            content: `:white_check_mark: aller hop !`,
+            ephemeral: true
+        });
     }
 }
