c0ebe26996e7ba293ff354309d80afe2e7c305e8
Youri Duret <youri.duret@gmail.com>
2025-01-21 10:51:58 +0100

    oublie role blacklist button

--- src/interactions/buttons/contribuer/secretariat_general.js
+++
@@ -1,8 +0,0 @@
-const {roles} = require("../../../api/permanent.js");
-const {rolereact} = require("../../../api/role.js");
-module.exports = {
-    customID: "secretariat_general",
-    runInteraction: async (client, interaction) => {
-        return rolereact(interaction, roles.secretariat_general);
-    }
-}
