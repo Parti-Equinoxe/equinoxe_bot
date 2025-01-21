ee2abfdfdbeaa4b06a97f54f7bb64501c29663e7
Youri Duret <youri.duret@gmail.com>
2025-01-21 10:51:19 +0100

    maj roles blacklist button

--- src/interactions/buttons/contribuer/reseau_territorial.js
+++
@@ -1,8 +0,0 @@
-const {roles} = require("../../../api/permanent.js");
-const {rolereact} = require("../../../api/role.js");
-module.exports = {
-    customID: "reseau_territorial",
-    runInteraction: async (client, interaction) => {
-        return rolereact(interaction, roles.reseau_territorial);
-    }
-}
--- src/interactions/buttons/contribuer/tire_au_sort.js
+++
@@ -1,8 +0,0 @@
-const {roles} = require("../../../api/permanent.js");
-const {rolereact} = require("../../../api/role.js");
-module.exports = {
-    customID: "tire_au_sort",
-    runInteraction: async (client, interaction) => {
-        return rolereact(interaction, roles.tire_au_sort);
-    }
-}
