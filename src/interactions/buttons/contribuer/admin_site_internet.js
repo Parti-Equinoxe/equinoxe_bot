73435843a0b8d212b04004fc0b4b3914a013fc59
Youri Duret <youri.duret@gmail.com>
2025-02-06 13:25:18 +0100

    rajout fichier supp par erreure

---
+++ src/interactions/buttons/contribuer/admin_site_internet.js
@@ -0,0 +1,10 @@
+const { MessageFlags}= require("discord.js");
+module.exports = {
+    customID: "admin_site_internet",
+    runInteraction: async (client, interaction) => {
+        return interaction.reply({
+            content: ":not_entry_sign: Seul les personnes ayant accès au panel admin du site ont se rôle, aller dans <#1268310816092979261> pour le demander.",
+            flags: [MessageFlags.Ephemeral]
+        })
+    }
+}
