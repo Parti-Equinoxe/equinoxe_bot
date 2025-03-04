b01259348c12c68d58805924a25b72db59164dbe
Youri Duret <youri.duret@gmail.com>
2025-03-04 12:32:02 +0100

    fix: orthographe

--- src/data/pre_load/comiteethique.js
+++ src/data/pre_load/comiteethique.js
@@ -11,7 +11,7 @@ module.exports.send = async (interaction) => {
     return {
         embeds: [
             new EmbedBuilder()
-                .setDescription("# Le <@&1307780177702551736> :\n> Le comité d’éthique est chargé de garantir **un environnement respectueux et inclusif** au sein d’Équinoxe.\n\nSi vous rencontrez un problème avec un adhérent ou une situation inconfortable, n’hésitez pas les contacter :\n### `comite-ethique@parti-equinoxe.fr`\nVous pouvez aussi allez leur posez des question sur Discord : <#1309909796883005590>.")
+                .setDescription("# Le <@&1307780177702551736> :\n> Le comité d’éthique est chargé de garantir **un environnement respectueux et inclusif** au sein d’Équinoxe.\n\nSi vous rencontrez un problème avec un adhérent ou une situation inconfortable, n’hésitez pas à les contacter :\n### `comite-ethique@parti-equinoxe.fr`\nVous pouvez aussi aller leur poser des questions sur Discord : <#1309909796883005590>.")
                 .setColor(couleurs.jaune)
                 .setThumbnail(banniere.link)
                 .setImage(`attachment://info.png`)
