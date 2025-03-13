fa5b23469c88395ac7347b72f5506c375b28bf5b
youritch <youri.duret@gmail.com>
2025-03-13 20:50:10 +0100

    fix: donne le role sympathisant en arrivant sur le serv

--- src/events/client/arriver.js
+++ src/events/client/arriver.js
@@ -1,8 +1,10 @@
 const {client} = require("../../index.js");
 const {Events} = require("discord.js");
 const msg = require("../../data/pre_load/accueil.js");
+const {roles} = require("../../api/permanent");
 
 client.on(Events.GuildMemberAdd, async (member) => {
+    member.roles.add(roles.sympathisant);
     const channel = await member.createDM(true).catch(() => null);
     if (!channel) return;
     await channel.send(await msg.send().catch(() => null)).catch(() => null);
