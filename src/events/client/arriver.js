f6c96ed66ae24b8695c358bd7d9424765666ee1d
youritch <youri.duret@gmail.com>
2025-03-14 08:30:16 +0100

    fix: oubli d'un await

--- src/events/client/arriver.js
+++ src/events/client/arriver.js
@@ -4,7 +4,7 @@ const msg = require("../../data/pre_load/accueil.js");
 const {roles} = require("../../api/permanent");
 
 client.on(Events.GuildMemberAdd, async (member) => {
-    member.roles.add(roles.sympathisant);
+    await member.roles.add(roles.sympathisant);
     const channel = await member.createDM(true).catch(() => null);
     if (!channel) return;
     await channel.send(await msg.send().catch(() => null)).catch(() => null);
